1b1aa09c3f6f7511facfdcd0241609c1401d2bab
Rabbear <38034358+RabbearSu@users.noreply.github.com>
2019-12-07 14:53:17 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+(function(angular) {
+  var app = angular.module('myApp', [
+    'ngRoute',
+    'moviecat.movie_list'
+  ])
+
+  app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.otherwise({
+    	redirectTo: '/in_theaters/1'
+    })
+  }])
+
+  //聚焦功能
+  app.controller('myController', [
+    '$scope',
+    '$location',
+    function($scope, $location) {
+      //因为$watch只能监听$scope中的变量，所以要挂载
+      $scope.$location = $location
+
+      $scope.$watch('$location.path()', function (now, old) {
+        $scope.category = now.replace(/\/(.+?)\/\d+/, '$1')
+        // console.log($scope.category)
+      })
+  }])
+})(angular)
